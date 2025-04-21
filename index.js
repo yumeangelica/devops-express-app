a8b38058983814b8bd11001ced7078230a3f871f
yumeangelica <74900808+yumeangelica@users.noreply.github.com>
2025-04-21 04:33:39 +0300

    Up

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('Hello from DevOps with Docker!</h1>')
+  res.send('Hello again from DevOps with Docker!</h1>')
 })
 
 app.get('/ping', (req, res) => {
